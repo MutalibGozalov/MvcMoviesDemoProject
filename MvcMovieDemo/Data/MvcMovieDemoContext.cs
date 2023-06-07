89f37b3473f803d9429f75ff7c4e2ccd45d2fcab
Mutalib SH. Gozalov <mutalibshg@code.local>
2023-06-07 11:07:14 +0400

    Project initiated, seed data added to db

---
+++ MvcMovieDemo/Data/MvcMovieDemoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovieDemo.Models;
+
+namespace MvcMovieDemo.Data
+{
+    public class MvcMovieDemoContext : DbContext
+    {
+        public MvcMovieDemoContext (DbContextOptions<MvcMovieDemoContext> options) : base(options)
+        {
+        }
+
+        public DbSet<MvcMovieDemo.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ MvcMovieDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcMovieDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
